03bf6f2cfdb0afe0c96fff399a6bb5bda57d0dbe
gudmundurste12 <gudmundurste12@ru.is>
2014-09-11 08:56:03 +0000

    Added android as an external library

---
+++ src/com/FlowFree/MainActivity.java
@@ -0,0 +1,14 @@
+package com.FlowFree;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Gvendur Stefáns on 11.9.2014.
+ */
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
