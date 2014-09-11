8337884b5f114d08f532612d696bb046ac23fa55
kristinnv12 <kristinnv12@ru.is>
2014-09-11 10:54:59 +0000

    options

---
+++ src/com/example/FlowFree/OptionsActivity.java
@@ -0,0 +1,18 @@
+package com.example.FlowFree;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kristinn on 11.9.2014.
+ */
+public class OptionsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options);
+    }
+
+}
