034dbcfde73c5d3b77792b75dd195185868529c0
gudmundurste12 <gudmundurste12@ru.is>
2014-09-11 09:18:12 +0000

    Copied the teacher's work as a starting point

---
+++ src/com/example/ATracePath/PlayActivity.java
@@ -0,0 +1,24 @@
+package com.example.ATracePath;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+
+/**
+ * Created by Gvendur Stefáns on 8.9.2014.
+ */
+public class PlayActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.play);
+
+		SharedPreferences settings = getSharedPreferences("ColorPref", MODE_PRIVATE);
+
+		int color = settings.getInt("pathColor", Color.CYAN);
+
+		Board board = (Board)findViewById(R.id.board);
+		board.setColor(color);
+	}
+}
